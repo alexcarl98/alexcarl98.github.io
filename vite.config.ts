8a6853f6f2d6e76610d8d9a5c2f6bc8a8935e3fc
Alex Alvarez <alex.alvarez1903@gmail.com>
2025-03-30 18:28:11 -0700

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // for user site (username.github.io)
+  base: '/',
   // if this was a project site, you'd use: base: '/repository-name/'
 })
